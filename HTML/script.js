8bd5fd2921ec03141c884751085773b55eb99525
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2022-02-22 01:49:53 +0300

    Update script.js

--- HTML/script.js
+++ HTML/script.js
@@ -1,22 +1,21 @@
-
-var menulist = document.getElementById("menulist");
-//var nav = document.getElementsById("nav");
-menulist.style.maxHeight= "0px";
-
-    function togglemenu(){
-        if(menulist.style.maxHeight == "0px"){
-            menulist.style.maxHeight = "130px";
-
-        }
-        else{
-            menulist.style.maxHeight = "0px";
-        }
-
-        
-    }
-
     window.addEventListener("scroll" , function(){
         var nav = document.getElementById("nav");
         
         nav.classList.toggle("sticky" ,window.scrollY > 0)
     })
+
+    var ul = document.getElementById("menulist");
+    var lbut=document.getElementById("l");
+    var xbut = document.getElementById("x");
+    
+    function togglemenu(){
+        ul.style.left = "0px";
+        lbut.style.display = "none";
+        xbut.style.display = "inline";
+    }
+
+    function cancel(){
+        ul.style.left = "-1230px";
+        lbut.style.display = "inline";
+        xbut.style.display = "none";
+    }
